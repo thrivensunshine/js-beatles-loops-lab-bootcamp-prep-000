86488eec715b7f0841a7e01b54cc5bc6aae3c0ca
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-06 14:34:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// add solution here
+
+function theBeatlesPlay(arrM, arrI){
+let finArr= []
+//console.log(arrI.length)
+for(let i = 0; i < arrM.length; i++){
+  for(let j = 0; j < arrI.length; j++){
+    if(i === j){
+      finArr.push(`${arrM[i]} plays ${arrI[j]}`)
+    }
+  }
+}return finArr
+}
+
+function johnLennonFacts(arr)  {
+  
+let fin =''
+let finArr = []
+var i = 0;
+
+  while(i < arr.length-1) {
+  i++
+  fin = arr[i] + "!!!"
+  finArr.push(arr[0])
+  finArr.push(fin)
+  finArr.pop()
+
+//console.log(finArr);
+
+}return finArr
+  }
