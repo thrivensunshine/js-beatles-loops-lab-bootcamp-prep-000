3d7f446a49642288494fa113686ad1afa7de6843
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-06 15:06:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,20 +11,23 @@ for(let i = 0; i < arrM.length; i++){
 }return finArr
 }
 
-function johnLennonFacts(arr)  {
-  
+  function johnLennonFacts(arr)  {
 let fin =''
+let exla = ''
 let finArr = []
 var i = 0;
-
-  while(i < arr.length-1) {
+while(arr.length- 1 >= i || i === 0) {
+  console.log(i + "  <-- 1" )
+  console.log( arr[i])
   i++
-  fin = arr[i] + "!!!"
-  finArr.push(arr[0])
-  finArr.push(fin)
-  finArr.pop()
+  fin = arr[i]
+  exla = "!!!"
+  //finArr.push(arr[0]+"!!!")
+  finArr.push(fin + exla)
+  //finArr.pop()
 
 //console.log(finArr);
 
 }return finArr
   }
+
